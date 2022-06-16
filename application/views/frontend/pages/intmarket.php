56b1210393910258a29e51da66a5c0cd9a1e3dde
msyaban890 <msyaban890@gmail.com>
2022-06-16 17:23:08 +0700

    intermarket-dummy

---
+++ application/views/frontend/pages/intmarket.php
@@ -0,0 +1,118 @@
+<!--Breadcrumb Area-->
+<section class="breadcrumb-area banner-2" data-background="<?= base_url('assets/') ?>images/banner/9.jpg">
+	<div class="text-block">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 v-center">
+					<div class="bread-inner">
+						<div class="bread-menu">
+							<ul>
+								<li><a href="index.html">Niwax</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+						</div>
+						<div class="bread-title">
+							<h2>Internet Marketing</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--End Breadcrumb Area-->
+<!--Start Privacy Policy-->
+<section class="dg-service2 dg-bg--2 pb130 pt130">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+
+				<div class="row">
+					<div class="col-md-20">
+						<h4>Digital Marketing</h4>
+						<p class="mb10">Optimalkan pengelolaan social media anda untuk menjaga hubungan dengan customer anda dan tingkatkan bisnis anda.</p>
+						<hr>
+						<p class="mb10"> Ingin bisnis perusahaan Anda melesat dan mendapatkan konsumen lebih banyak? Gunakan jasa digital marketing professional dari tim Sribulancer. Anda hanya cukup menentukan budget dan memberikan arahan, tim kami akan menyelesaikannya dengan baik dan rasakan bisnis Anda mendominasi di internet.</p>
+
+					</div>
+					<div class="col-md-20">
+						<h4 class="mb10">Kenapa Anda butuh jasa digital marketing?</h4>
+						<p class="mb10">Tahukah Anda? Sekitar 32% penduduk Indonesia yang aktif menggunakan internet (sekitar 143 juta penduduk), memanfaatkan internet sebgai tempat untuk membeli barang dan jasa yang dibutuhkan sehari-hari. Ini artinya, bisnis Anda akan jauh mendapatkan prospek lebih banyak jika mampu mendominasi di internet bermodalkan teknik digital marketing yang dilakukan dengan tepat, akurat dan professional.</p>
+					</div>
+
+					<div class="col-md-20">
+						<h4 class="mb10">Apa yang Kami Kerjakan?</h4>
+						<p class="mb10">Dalam membantu digital Marketing perusahaan anda, kami melakukan banyak hal yang itu menjadi fokus layanan digital marketing kami. Beberapa diantaranya adalah:</p>
+						<div class="niwax-list">
+							<ul class="list-style- mb20">
+								<li>IKLAN GOOGLE
+									<p>Kami membantu anda memasang iklan yang tepat di Mesin Pencarian Google dan jaringannya sehingga impact-nya akan terasa terhadap target dari anda beriklan di Google.</p>
+								</li>
+								<li>IKLAN YOUTUBE
+									<p>Anda juga dapat menggunakan jasa kami untuk membantu anda memasang iklan di Situs Video Sharing Youtube. Kami akan membuat strategi supaya iklan anda benar-benar efektif.</p>
+								</li>
+								<li>GOOGLE BISNIS
+									<p>Kami juga dapat membantu anda mendaftarkan Bisnis anda di Google Bisnis dan juga di Google Maps, sehingga mitra dan customer anda dapat menemukan anda dengan muda.</p>
+								</li>
+								<li>IKLAN FACEBOOK
+									<p>Facebook menjadi salah satu media yang dapat anda gunakan untuk beriklan produk dan layanan anda, anda akan dibantu untuk memasang iklan facebook yang tepat.</p>
+								</li>
+								<li>IKLAN INSTAGRAM
+									<p>Selain Facebook, Iklankan bisnis anda di Instagram dengan menggunakan layanan dan jasa kami, sehingga anda tidak perlu repot lagi, kami bantu strateginya sehingga iklan anda benar-benar efektif</p>
+								</li>
+								<li>MEDIA PLACEMENT
+									<p>Kami juga dapat membantu memberitakan bisnis anda di media-media ternama di Indonesia, apakah dalam bentuk release pengumuman atau dalam bentuk advetorial sehingga produk dan perusahaan anda makin dikenal.</p>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<p>Segera hubungi kami untuk mendapatkan informasi lebih lanjut tentang layanan digital marketing ini dan juga jangan sungkan menghubungi kami untuk mendapatkan penawaran terbaik.</p>
+					<!-- <p><strong>You may disable cookies in your browser using the following instructions:</strong></p>
+                    <ul class="mt10 list-style-">
+                        <li><a href="#" target="_blank" rel="noreferrer">Internet Explorer</a></li>
+                        <li><a href="#" target="_blank" rel="noreferrer">Chrome</a></li>
+                        <li><a href="#" target="_blank" rel="noreferrer">Firefox</a></li>
+                        <li><a href="#" target="_blank" rel="noreferrer">Safari</a></li>
+                        <li>For any other browser, please directly consult the cookie management help information available on the Internet.</li>
+                    </ul>
+
+                    <h4 class="mb10 mt30">6. How We Use Cookies?</h4>
+
+                    <p class="mb10">There may be several online forms present on our website where you are able to submit personally identifiable information in order to express interest in our business or submit an inquiry regarding our business. These forms may appear as RFP forms, Contact Us forms, Wedding RFP forms, or other similar forms that request your information before submitting. When you contact us using a form on our website, we collect information that identifies you, such as your name, job title, business name, phone number, email address, and other personally identifiable information. Some of this information is required and other information is optional. This information is used to better address your inquiry and is not used for marketing purposes or shared with other third party companies. If you would prefer that we do not collect this information then simply do not fill out the form. If you have already submitted your information through a form and would like to be removed, please email us.</p>
+
+
+                    <h4 class="mb10 mt30">7. Email Communications</h4>
+
+
+                    <p class="mb10">Based upon the Personal Information that you provide us, we may communicate with you in response to your inquiries to provide the services you request and to manage your account. We will communicate with you by email or telephone, in accordance with your wishes. We may also use your Personal Information to send you updates and other promotional communications. If you no longer wish to receive those email updates, you may opt-out of receiving them by following the instructions included in each update or communication.</p>
+
+                    <p>Specifically, we use Niwax to store email addresses of those who have opted into our email marketing campaigns. We also use Niwax to distribute marketing emails to our list of opt-in subscribers. These emails may contain a pixel or a web beacon that identifies whether you have 1) received the email; 2) opened the email; or 3) clicked the email. You may opt out or unsubscribe from our marketing emails if you do not want us to collect this information from you. You may also email us if you would like to be removed. For more information, please review <a href="#" rel="noreferrer" target="_blank">Niwax's own Privacy Policy&#8203;</a>.</p>
+
+                    <h4 class="mb10 mt30">8. Online Advertising</h4>
+
+                    <p class="mb10">We participate in a variety of online advertising. This advertising displays our ads to you on the websites and apps that reside outside of this website. </p>
+
+                    <p><strong>We collect information that identifies: </strong></p>
+
+                    <ul class="list-style- mb10 mt10">
+                        <li>which ads are displayed</li>
+                        <li>which ads are clicked</li>
+                        <li>where the ad was displayed</li>
+                    </ul>
+
+                    <p>Our website is hosted by InMotion Hosting, Inc. InMotion Hosting provides us with the online platform that allows us to provide the website to you. Your information, including Personal Information, may be stored through InMotion Hosting servers. By using the website, you consent to InMotion Hosting’s collection, disclosure, storage, and use of your Personal Information in accordance with InMotion Hosting’s privacy policy. </p>
+
+
+                    <h4 class="mb10 mt30">9. Why You Collect Information?</h4>
+
+                    <p class="mb10">There may be several online forms present on our website where you are able to submit personally identifiable information in order to express interest in our business or submit an inquiry regarding our business. These forms may appear as RFP forms, Contact Us forms, Wedding RFP forms, or other similar forms that request your information before submitting. When you contact us using a form on our website, we collect information that identifies you, such as your name, job title, business name, phone number, email address, and other personally identifiable information. Some of this information is required and other information is optional. This information is used to better address your inquiry and is not used for marketing purposes or shared with other third party companies. If you would prefer that we do not collect this information then simply do not fill out the form. If you have already submitted your information through a form and would like to be removed, please email us.</p>
+
+                    <h4 class="mb10 mt30">Last Updated</h4>
+                    <p>This Privacy Policy was last updated on Wed, 01 Sep, 2021.</p> -->
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+</section>
+<!--End Privacy Policy-->
