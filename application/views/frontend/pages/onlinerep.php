16f460831b779254f3d1f7b0d6b12917de24199f
msyaban890 <msyaban890@gmail.com>
2022-06-16 17:28:07 +0700

    onlineReputation-dummy

---
+++ application/views/frontend/pages/onlinerep.php
@@ -0,0 +1,99 @@
+<!--Breadcrumb Area-->
+<section class="breadcrumb-area banner-2" data-background="<?= base_url('assets/') ?>images/banner/9.jpg">
+    <div class="text-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 v-center">
+                    <div class="bread-inner">
+                        <div class="bread-menu">
+                            <ul>
+                                <li><a href="index.html">Niwax</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                        <div class="bread-title">
+                            <h2>Online Reputation Management</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End Breadcrumb Area-->
+<!--Start Privacy Policy-->
+<section class="dg-service2 dg-bg--2 pb130 pt130">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <div class="row">
+                    <div class="col-md-20">
+                        <h4>Online Reputation Management </h4>
+                        <p class="mb10">Online Reputation Management merupakan cara atau strategi untuk mengontrol setiap aktivitas maupun timbal balik terhadap suatu brand, terutama dalam dunia digital. Lalu, kenapa bisnis Anda membutuhkannya dan apa saja jenisnya?</p>
+                        <hr>
+                        <p> Mengapa Bisnis Anda Membutuhkan Online Reputation Management?</p>
+                        <p class="mb10">Sebagai seorang pebisnis, Anda sangat membutuhkan ORM ini untuk menunjang perkembangan bisnis. Ada dua alasan utama yang memperkuatnya, yaitu sebagai berikut:</p>
+
+                    </div>
+                    <div class="col-md-20">
+                        <h4 class="mb10">Memperkuat Brand Reputation di Dunia Digital</h4>
+                        <p class="mb10">Membangun reputasi yang baik di dunia digital sangat penting bagi brand Anda. Alasannya, karena beberapa tahun belakangan telah lahir tren baru di mana kebanyakan orang memanfaatkan internet untuk mencari referensi akan produk maupun jasa yang mereka butuhkan terlebih dahulu, sebelum memutuskan melakukan transaksi pembelian. Dengan adanya ORM, produk, jasa, atau brand Anda menjadi lebih dikenal dan dipilih oleh konsumen.</p>
+                        <p class="mb10">Sederhananya, semakin kuat reputasi brand Anda di dunia digital, tentu akan meraih banyak hal positif, mulai peningkatan rating terkait kualitas produk berdasarkan pengalaman pengguna, peningkatan kepercayaan konsumen terhadap produk dari brand Anda, bahkan bisa terjadi peningkatan penjualan dan pertambahan jumlah pelanggan aktif secara signifikan.</p>
+                    </div>
+
+                    <div class="col-md-20">
+                        <h4 class="mb10">Manajemen Komentar Negatif</h4>
+                        <p class="mb10">Selain memperkuat reputasi baik brand, ORM juga dapat mengatasi munculnya komentar negatif dari segelintir konsumen yang tidak puas, bahkan bisa jadi kompetitor yang memberikan komentar negatif. Tentu hal ini tidak bisa dihindari, karena persaingan bisnis yang semakin ketat dalam segala bidang. Maka jangan heran jika terjadi black campaign atau isu-isu negatif tentang bisnis Anda.</p>
+                        <p class="mb10">Namun jangan khawatir. Adanya ORM, dapat digunakan untuk memulihkan kembali reputasi lewat berbagai strategi, di antaranya bekerja sama dengan digital agency Jakarta dalam pengadaan content marketing, bekerja sama dengan pegiat media sosial dan influencer, menerapkan SEO, dan sebagainya.</p>
+                    </div>
+
+                    <!-- <p><strong>You may disable cookies in your browser using the following instructions:</strong></p>
+                    <ul class="mt10 list-style-">
+                        <li><a href="#" target="_blank" rel="noreferrer">Internet Explorer</a></li>
+                        <li><a href="#" target="_blank" rel="noreferrer">Chrome</a></li>
+                        <li><a href="#" target="_blank" rel="noreferrer">Firefox</a></li>
+                        <li><a href="#" target="_blank" rel="noreferrer">Safari</a></li>
+                        <li>For any other browser, please directly consult the cookie management help information available on the Internet.</li>
+                    </ul>
+
+                    <h4 class="mb10 mt30">6. How We Use Cookies?</h4>
+
+                    <p class="mb10">There may be several online forms present on our website where you are able to submit personally identifiable information in order to express interest in our business or submit an inquiry regarding our business. These forms may appear as RFP forms, Contact Us forms, Wedding RFP forms, or other similar forms that request your information before submitting. When you contact us using a form on our website, we collect information that identifies you, such as your name, job title, business name, phone number, email address, and other personally identifiable information. Some of this information is required and other information is optional. This information is used to better address your inquiry and is not used for marketing purposes or shared with other third party companies. If you would prefer that we do not collect this information then simply do not fill out the form. If you have already submitted your information through a form and would like to be removed, please email us.</p>
+
+
+                    <h4 class="mb10 mt30">7. Email Communications</h4>
+
+
+                    <p class="mb10">Based upon the Personal Information that you provide us, we may communicate with you in response to your inquiries to provide the services you request and to manage your account. We will communicate with you by email or telephone, in accordance with your wishes. We may also use your Personal Information to send you updates and other promotional communications. If you no longer wish to receive those email updates, you may opt-out of receiving them by following the instructions included in each update or communication.</p>
+
+                    <p>Specifically, we use Niwax to store email addresses of those who have opted into our email marketing campaigns. We also use Niwax to distribute marketing emails to our list of opt-in subscribers. These emails may contain a pixel or a web beacon that identifies whether you have 1) received the email; 2) opened the email; or 3) clicked the email. You may opt out or unsubscribe from our marketing emails if you do not want us to collect this information from you. You may also email us if you would like to be removed. For more information, please review <a href="#" rel="noreferrer" target="_blank">Niwax's own Privacy Policy&#8203;</a>.</p>
+
+                    <h4 class="mb10 mt30">8. Online Advertising</h4>
+
+                    <p class="mb10">We participate in a variety of online advertising. This advertising displays our ads to you on the websites and apps that reside outside of this website. </p>
+
+                    <p><strong>We collect information that identifies: </strong></p>
+
+                    <ul class="list-style- mb10 mt10">
+                        <li>which ads are displayed</li>
+                        <li>which ads are clicked</li>
+                        <li>where the ad was displayed</li>
+                    </ul>
+
+                    <p>Our website is hosted by InMotion Hosting, Inc. InMotion Hosting provides us with the online platform that allows us to provide the website to you. Your information, including Personal Information, may be stored through InMotion Hosting servers. By using the website, you consent to InMotion Hosting’s collection, disclosure, storage, and use of your Personal Information in accordance with InMotion Hosting’s privacy policy. </p>
+
+
+                    <h4 class="mb10 mt30">9. Why You Collect Information?</h4>
+
+                    <p class="mb10">There may be several online forms present on our website where you are able to submit personally identifiable information in order to express interest in our business or submit an inquiry regarding our business. These forms may appear as RFP forms, Contact Us forms, Wedding RFP forms, or other similar forms that request your information before submitting. When you contact us using a form on our website, we collect information that identifies you, such as your name, job title, business name, phone number, email address, and other personally identifiable information. Some of this information is required and other information is optional. This information is used to better address your inquiry and is not used for marketing purposes or shared with other third party companies. If you would prefer that we do not collect this information then simply do not fill out the form. If you have already submitted your information through a form and would like to be removed, please email us.</p>
+
+                    <h4 class="mb10 mt30">Last Updated</h4>
+                    <p>This Privacy Policy was last updated on Wed, 01 Sep, 2021.</p> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<!--End Privacy Policy-->
