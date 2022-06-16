6bcfe0c29de02d4816fd60cf9831327a60379edc
msyaban890 <msyaban890@gmail.com>
2022-06-16 17:17:41 +0700

    booster-dummy

---
+++ application/views/frontend/pages/boostersosmed.php
@@ -0,0 +1,183 @@
+<!--Breadcrumb Area-->
+<section class="breadcrumb-area banner-2" data-background="<?= base_url('assets/') ?>images/banner/9.jpg">
+    <div class="text-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 v-center">
+                    <div class="bread-inner">
+                        <div class="bread-menu">
+                            <ul>
+                                <li><a href="index.html">Niwax</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                        <div class="bread-title">
+                            <h2>Booster Sosial Media</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End Breadcrumb Area-->
+<!--Start Privacy Policy-->
+<section class="dg-service2 dg-bg--2 pb130 pt130">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <div class="row">
+                    <div class="col-md-20">
+                        <h4>Paket Booster Media Social</h4>
+                        <p class="mb10">Tingkatkan kredibilitas usaha Anda dengan meningkatkan follower ataupun viewer di Media Social. Kami siapkan paket booster Media Social yang sesuai untuk Anda</p>
+                        <hr>
+                    </div>
+                    <!--Start Faqs-->
+                    <section class="dg-service2 dg-bg--2">
+                        <div class="container">
+                            <div class="row">
+                                <div class="accordion booster-package" id="accordionFlushExample">
+                                    <div class="accordion-item facebook">
+                                        <h2 class="accordion-header" id="flush-headingOne">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                                Paket Facebook Booster
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                                        </div>
+                                    </div>
+                                    <div class="accordion-item instagram">
+                                        <h2 class="accordion-header" id="flush-headingTwo">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                                Paket Instagram Booster
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                                        </div>
+                                    </div>
+                                    <div class="accordion-item tiktok">
+                                        <h2 class="accordion-header" id="flush-headingThree">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                                Paket Tiktok Booster
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                                        </div>
+                                    </div>
+                                    <div class="accordion-item twitter">
+                                        <h2 class="accordion-header" id="flush-heading-1">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-1" aria-expanded="false" aria-controls="flush-collapseFourth">
+                                                Paket Twitter Booster
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapse-1" class="accordion-collapse collapse" aria-labelledby="flush-heading-1" data-bs-parent="#accordionFlushExample">
+                                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                                        </div>
+                                    </div>
+                                    <div class="accordion-item youtube">
+                                        <h2 class="accordion-header" id="flush-heading-2">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-2" aria-expanded="false" aria-controls="flush-collapseFifth">
+                                                Paket Youtube Booster
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapse-2" class="accordion-collapse collapse" aria-labelledby="flush-heading-2" data-bs-parent="#accordionFlushExample">
+                                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- <div class="col-md-6 mt60">
+                                    <div class="accordion" id="accordionExample-a">
+                                        <div class="accordion-item bdr0">
+                                            <h2 class="accordion-header" id="heading-b">
+                                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1a" aria-expanded="true" aria-controls="collapseOne">
+                                                    Incomprehensibility or readability? That is the question.
+                                                </button>
+                                            </h2>
+                                            <div id="collapse-1a" class="accordion-collapse collapse show" aria-labelledby="heading-b" data-bs-parent="#accordionExample-a">
+                                                <div class="accordion-body">
+                                                    <div class="data-reqs">
+                                                        <h5 class="pb20">Content List</h5>
+                                                        <p>5 Years Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="accordion-item bdr0">
+                                            <h2 class="accordion-header" id="heading-c">
+                                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-2a" aria-expanded="true" aria-controls="collapseOne">
+                                                    Origin and meaning of the Lorem Ipsum text
+                                                </button>
+                                            </h2>
+                                            <div id="collapse-2a" class="accordion-collapse collapse" aria-labelledby="heading-c" data-bs-parent="#accordionExample-a">
+                                                <div class="accordion-body">
+                                                    <div class="data-reqs">
+                                                        <h5 class="pb20">Content List</h5>
+                                                        <p>5 Years Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </section>
+                    <!--End Faqs-->
+
+
+
+
+                    <!-- <p><strong>You may disable cookies in your browser using the following instructions:</strong></p>
+                    <ul class="mt10 list-style-">
+                        <li><a href="#" target="_blank" rel="noreferrer">Internet Explorer</a></li>
+                        <li><a href="#" target="_blank" rel="noreferrer">Chrome</a></li>
+                        <li><a href="#" target="_blank" rel="noreferrer">Firefox</a></li>
+                        <li><a href="#" target="_blank" rel="noreferrer">Safari</a></li>
+                        <li>For any other browser, please directly consult the cookie management help information available on the Internet.</li>
+                    </ul>
+
+                    <h4 class="mb10 mt30">6. How We Use Cookies?</h4>
+
+                    <p class="mb10">There may be several online forms present on our website where you are able to submit personally identifiable information in order to express interest in our business or submit an inquiry regarding our business. These forms may appear as RFP forms, Contact Us forms, Wedding RFP forms, or other similar forms that request your information before submitting. When you contact us using a form on our website, we collect information that identifies you, such as your name, job title, business name, phone number, email address, and other personally identifiable information. Some of this information is required and other information is optional. This information is used to better address your inquiry and is not used for marketing purposes or shared with other third party companies. If you would prefer that we do not collect this information then simply do not fill out the form. If you have already submitted your information through a form and would like to be removed, please email us.</p>
+
+
+                    <h4 class="mb10 mt30">7. Email Communications</h4>
+
+
+                    <p class="mb10">Based upon the Personal Information that you provide us, we may communicate with you in response to your inquiries to provide the services you request and to manage your account. We will communicate with you by email or telephone, in accordance with your wishes. We may also use your Personal Information to send you updates and other promotional communications. If you no longer wish to receive those email updates, you may opt-out of receiving them by following the instructions included in each update or communication.</p>
+
+                    <p>Specifically, we use Niwax to store email addresses of those who have opted into our email marketing campaigns. We also use Niwax to distribute marketing emails to our list of opt-in subscribers. These emails may contain a pixel or a web beacon that identifies whether you have 1) received the email; 2) opened the email; or 3) clicked the email. You may opt out or unsubscribe from our marketing emails if you do not want us to collect this information from you. You may also email us if you would like to be removed. For more information, please review <a href="#" rel="noreferrer" target="_blank">Niwax's own Privacy Policy&#8203;</a>.</p>
+
+                    <h4 class="mb10 mt30">8. Online Advertising</h4>
+
+                    <p class="mb10">We participate in a variety of online advertising. This advertising displays our ads to you on the websites and apps that reside outside of this website. </p>
+
+                    <p><strong>We collect information that identifies: </strong></p>
+
+                    <ul class="list-style- mb10 mt10">
+                        <li>which ads are displayed</li>
+                        <li>which ads are clicked</li>
+                        <li>where the ad was displayed</li>
+                    </ul>
+
+                    <p>Our website is hosted by InMotion Hosting, Inc. InMotion Hosting provides us with the online platform that allows us to provide the website to you. Your information, including Personal Information, may be stored through InMotion Hosting servers. By using the website, you consent to InMotion Hosting’s collection, disclosure, storage, and use of your Personal Information in accordance with InMotion Hosting’s privacy policy. </p>
+
+
+                    <h4 class="mb10 mt30">9. Why You Collect Information?</h4>
+
+                    <p class="mb10">There may be several online forms present on our website where you are able to submit personally identifiable information in order to express interest in our business or submit an inquiry regarding our business. These forms may appear as RFP forms, Contact Us forms, Wedding RFP forms, or other similar forms that request your information before submitting. When you contact us using a form on our website, we collect information that identifies you, such as your name, job title, business name, phone number, email address, and other personally identifiable information. Some of this information is required and other information is optional. This information is used to better address your inquiry and is not used for marketing purposes or shared with other third party companies. If you would prefer that we do not collect this information then simply do not fill out the form. If you have already submitted your information through a form and would like to be removed, please email us.</p>
+
+                    <h4 class="mb10 mt30">Last Updated</h4>
+                    <p>This Privacy Policy was last updated on Wed, 01 Sep, 2021.</p> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<!--End Privacy Policy-->
